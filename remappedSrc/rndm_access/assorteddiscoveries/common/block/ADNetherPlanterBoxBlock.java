bd0250317815f7ac198155975f1011a2f5cf7dbe
rndmaccess <rndmaccessgaming@gmail.com>
2022-05-31 22:37:49 -0600

    Initial Commit?

--- remappedSrc/rndm_access/assorteddiscoveries/common/block/ADNetherPlanterBoxBlock.java
+++
@@ -1,9 +0,0 @@
-package rndm_access.assorteddiscoveries.common.block;
-
-import net.minecraft.block.AbstractBlock;
-
-public class ADNetherPlanterBoxBlock extends ADPlanterBoxBlock {
-    public ADNetherPlanterBoxBlock(AbstractBlock.Settings settings) {
-        super(settings);
-    }
-}

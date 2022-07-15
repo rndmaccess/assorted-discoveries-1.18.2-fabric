12d7fdc90bbf33e7be9ff575f749cde0248b1924
rndmaccess <rndmaccessgaming@gmail.com>
2022-07-14 19:21:00 -0600

    Cleaned up some code.

--- src/main/java/rndm_access/assorteddiscoveries/common/worldgen/feature/ADBloodKelpFeature.java
+++ src/main/java/rndm_access/assorteddiscoveries/common/worldgen/feature/ADBloodKelpFeature.java
@@ -3,13 +3,12 @@ package rndm_access.assorteddiscoveries.common.worldgen.feature;
 import com.mojang.serialization.Codec;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.KelpBlock;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.KelpFeature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
 import rndm_access.assorteddiscoveries.common.block.ADBloodKelpBlock;
 import rndm_access.assorteddiscoveries.common.block.ADBloodKelpPlantBlock;
@@ -24,53 +23,40 @@ public class ADBloodKelpFeature extends Feature<DefaultFeatureConfig> {
     }
 
     public boolean generate(FeatureContext<DefaultFeatureConfig> context) {
-        int i = 0;
-        StructureWorldAccess structureWorldAccess = context.getWorld();
-        BlockPos blockPos = context.getOrigin();
+        StructureWorldAccess world = context.getWorld();
+        BlockPos originPos = context.getOrigin();
         Random random = context.getRandom();
-        int y = structureWorldAccess.getTopY(Heightmap.Type.OCEAN_FLOOR, blockPos.getX(), blockPos.getZ());
-        BlockPos blockPos2 = new BlockPos(blockPos.getX(), y, blockPos.getZ());
+        int x = originPos.getX();
+        int z = originPos.getZ();
+        int y = world.getTopY(Heightmap.Type.OCEAN_FLOOR, x, z);
+        BlockPos placementPos = new BlockPos(x, y, z);
+        boolean isWater = world.getFluidState(placementPos).isOf(Fluids.WATER);
 
-        if (structureWorldAccess.getBlockState(blockPos2).isOf(Blocks.WATER)) {
+        if (isWater) {
             BlockState bloodKelpState = ADBlocks.BLOOD_KELP.getDefaultState();
-            ADBloodKelpPlantBlock bloodKelpPlantState = (ADBloodKelpPlantBlock) ADBlocks.BLOOD_KELP_PLANT;
-            int maxHeight = 1 + random.nextInt(10);
+            BlockState bloodKelpPlantState = ADBlocks.BLOOD_KELP_PLANT.getDefaultState();
+            int maxLength = 1 + random.nextInt(10);
 
-            for(int l = 0; l <= maxHeight; ++l) {
-                if (structureWorldAccess.getBlockState(blockPos2).isOf(Blocks.WATER)
-                        && structureWorldAccess.getBlockState(blockPos2.up()).isOf(Blocks.WATER)
-                        && bloodKelpPlantState.getDefaultState().canPlaceAt(structureWorldAccess, blockPos2)) {
-                    if (l == maxHeight) {
-                        structureWorldAccess.setBlockState(blockPos2, getBloodKelpState(random)
-                                .with(KelpBlock.AGE, random.nextInt(4) + 20), 2);
-                        ++i;
+            for(int length = 0; length <= maxLength; ++length) {
+                boolean canSustainBloodKelpPlant = bloodKelpPlantState.canPlaceAt(world, placementPos);
+                boolean isAboveEmpty = world.getFluidState(placementPos.up()).isEmpty();
+
+                if (canSustainBloodKelpPlant && isWater) {
+                    // Place blood kelp plant body blocks then place the stem block on top.
+                    if (isAboveEmpty || length == maxLength) {
+                        world.setBlockState(placementPos, bloodKelpState
+                                .with(ADBloodKelpBlock.LIT, ADBlockStateUtil.isBloodKelpLit(random))
+                                .with(ADBloodKelpBlock.AGE, random.nextInt(4) + 20), 2);
+                        return true;
                     } else {
-                        structureWorldAccess.setBlockState(blockPos2, getBloodKelpPlantState(random), 2);
+                        world.setBlockState(placementPos, bloodKelpPlantState
+                                .with(ADBloodKelpPlantBlock.LIT, ADBlockStateUtil.isBloodKelpLit(random)), 2);
                     }
-                } else if (l > 0) {
-                    BlockPos blockPos3 = blockPos2.down();
-                    if (bloodKelpState.canPlaceAt(structureWorldAccess, blockPos3) && !structureWorldAccess.getBlockState(blockPos3.down()).isOf(Blocks.KELP)) {
-                        structureWorldAccess.setBlockState(blockPos3, getBloodKelpState(random)
-                                .with(KelpBlock.AGE, random.nextInt(4) + 20), 2);
-                        ++i;
-                    }
-                    break;
                 }
-                blockPos2 = blockPos2.up();
+                placementPos = placementPos.up();
+                isWater = world.getFluidState(placementPos).isOf(Fluids.WATER);
             }
         }
-        return i > 0;
-    }
-
-    private static BlockState getBloodKelpPlantState(Random random) {
-        BlockState bloodKelpPlant = ADBlocks.BLOOD_KELP_PLANT.getDefaultState();
-
-        return bloodKelpPlant.with(ADBloodKelpPlantBlock.LIT, ADBlockStateUtil.isBloodKelpLit(random));
-    }
-
-    private static BlockState getBloodKelpState(Random random) {
-        BlockState bloodKelp = ADBlocks.BLOOD_KELP.getDefaultState();
-
-        return bloodKelp.with(ADBloodKelpPlantBlock.LIT, ADBlockStateUtil.isBloodKelpLit(random));
+        return false;
     }
 }

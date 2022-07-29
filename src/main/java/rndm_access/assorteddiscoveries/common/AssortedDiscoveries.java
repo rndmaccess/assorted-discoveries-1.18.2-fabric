f9e3489e18007fac04429b810e2f0256690176a2
rndmaccess <rndmaccessgaming@gmail.com>
2022-07-28 23:51:10 -0600

    Re-wrote the pine cone drop code. Now pine cones are inserted directly into the spruce leaves loot table.

--- src/main/java/rndm_access/assorteddiscoveries/common/AssortedDiscoveries.java
+++ src/main/java/rndm_access/assorteddiscoveries/common/AssortedDiscoveries.java
@@ -5,8 +5,19 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.block.Blocks;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.condition.TableBonusLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.GenerationStep;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +28,7 @@ public class AssortedDiscoveries implements ModInitializer {
 	public static final ItemGroup MOD_GROUP = FabricItemGroupBuilder.build(ADReference.makeId("mod_group"),
 			() -> new ItemStack(ADItems.CALCITE_BRICKS));
 	public static final Logger LOGGER = LoggerFactory.getLogger(ADReference.MOD_ID);
+	private static final Identifier SPRUCE_LEAVES_LOOT_TABLE = Blocks.SPRUCE_LEAVES.getLootTableId();
 
 	@Override
 	public void onInitialize() {
@@ -31,6 +43,7 @@ public class AssortedDiscoveries implements ModInitializer {
 		ADPaintingTypes.registerPaintingMotives();
 		ADSoundEvents.registerSoundEvents();
 		AssortedDiscoveries.registerFuel();
+		AssortedDiscoveries.modifyLootTables();
 
 		// Entity Registries
 		ADVillagerTypes.registerVillagerTypes();
@@ -72,5 +85,19 @@ public class AssortedDiscoveries implements ModInitializer {
 
 	private static void registerFuel() {
 		FuelRegistry.register(4000, ADItems.DRIED_BLOOD_KELP_BLOCK);
+	}
+
+	private static void modifyLootTables() {
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if(source.isBuiltin() && SPRUCE_LEAVES_LOOT_TABLE.equals(id)) {
+				LootPool.Builder poolBuilder = LootPool.builder()
+						.rolls(ConstantLootNumberProvider.create(1))
+						.conditionally(TableBonusLootCondition.builder(Enchantments.FORTUNE, 0.02F, 0.023F, 0.025F, 0.035F, 0.1F))
+						.with(ItemEntry.builder(ADItems.SPRUCE_CONE))
+						.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0F, 2.0F)));
+
+				tableBuilder.pool(poolBuilder);
+			}
+		});
 	}
 }

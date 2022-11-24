6dbccea0ef7c74854fb384da730672d755551942
rndmaccess <rndmaccessgaming@gmail.com>
2022-11-24 13:56:02 -0700

    Added the bamboo fences to the cuttout blocks.

--- src/main/java/rndm_access/assorteddiscoveries/client/AssortedDiscoveriesClient.java
+++ src/main/java/rndm_access/assorteddiscoveries/client/AssortedDiscoveriesClient.java
@@ -169,7 +169,8 @@ public class AssortedDiscoveriesClient implements ClientModInitializer {
                 ADBlocks.BLUEBERRY_BUSH, ADBlocks.WITCHS_CRADLE, ADBlocks.SNAPDRAGON, ADBlocks.POTTED_SNAPDRAGON,
                 ADBlocks.ENDER_GRASS, ADBlocks.ICICLE, ADBlocks.CATTAIL, ADBlocks.POTTED_PURPLE_MUSHROOM,
                 ADBlocks.BLOOD_KELP, ADBlocks.BLOOD_KELP_PLANT, ADBlocks.WEEPING_HEART,
-                ADBlocks.MAROON_CAMPFIRE, ADBlocks.MAROON_TORCH, ADBlocks.MAROON_WALL_TORCH, ADBlocks.MAROON_LANTERN);
+                ADBlocks.MAROON_CAMPFIRE, ADBlocks.MAROON_TORCH, ADBlocks.MAROON_WALL_TORCH, ADBlocks.MAROON_LANTERN,
+                ADBlocks.DRIED_BAMBOO_FENCE_GATE, ADBlocks.FRESH_BAMBOO_FENCE_GATE);
 
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutoutMipped(),
                 ADBlocks.ENDERMAN_PLUSH, ADBlocks.GRASS_SLAB);

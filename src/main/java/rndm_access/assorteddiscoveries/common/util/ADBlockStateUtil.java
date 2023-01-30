f7448a60f5bb3d3c9211eb8eeda0932ef5ebd421
rndmaccess <rndm.access.dev@gmail.com>
2023-01-29 20:48:43 -0700

    Cleaned up the blood kelp code.

--- src/main/java/rndm_access/assorteddiscoveries/common/util/ADBlockStateUtil.java
+++ src/main/java/rndm_access/assorteddiscoveries/common/util/ADBlockStateUtil.java
@@ -6,17 +6,9 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.world.WorldAccess;
 import rndm_access.assorteddiscoveries.common.core.CBlockTags;
 
-import java.util.Random;
-
 public final class ADBlockStateUtil {
     public static boolean isSnowSlabOrStairs(WorldAccess world, BlockPos pos, BlockState state) {
         boolean isCovered = state.isSideSolidFullSquare(world, pos, Direction.DOWN);
         return state.isIn(CBlockTags.SNOW_STAIRS) && isCovered || state.isIn(CBlockTags.SNOW_SLABS) && isCovered;
-    }
-
-    public static boolean isBloodKelpLit(Random random) {
-        float randomNumPicked = random.nextFloat();
-
-        return randomNumPicked < 0.3F;
     }
 }

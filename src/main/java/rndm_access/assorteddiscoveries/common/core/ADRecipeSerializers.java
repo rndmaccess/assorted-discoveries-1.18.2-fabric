e5d0d6da3530e7692ed3f17b74ff1de09650e56d
rndmaccess <rndm.access.dev@gmail.com>
2023-03-13 15:42:01 -0600

    Cleaned up the woodcutter recipe code and consolidated it into 1 class.

--- src/main/java/rndm_access/assorteddiscoveries/common/core/ADRecipeSerializers.java
+++ src/main/java/rndm_access/assorteddiscoveries/common/core/ADRecipeSerializers.java
@@ -3,15 +3,13 @@ package rndm_access.assorteddiscoveries.common.core;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeSerializer;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import rndm_access.assorteddiscoveries.ADReference;
 import rndm_access.assorteddiscoveries.common.AssortedDiscoveries;
-import rndm_access.assorteddiscoveries.common.item.crafting.ADCuttingRecipe;
 import rndm_access.assorteddiscoveries.common.item.crafting.ADWoodcuttingRecipe;
 
 public class ADRecipeSerializers {
-    public static final RecipeSerializer<ADWoodcuttingRecipe> WOODCUTTING = new ADCuttingRecipe.Serializer<>(ADWoodcuttingRecipe::new);
+    public static final RecipeSerializer<ADWoodcuttingRecipe> WOODCUTTING = new ADWoodcuttingRecipe.Serializer<>(ADWoodcuttingRecipe::new);
 
     public static void registerSerializers() {
         register("woodcutting", WOODCUTTING);
